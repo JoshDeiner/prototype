32ef262869fbdd4df36156bce468b17f58ab561b
Josh Deiner <44216007+JoshDeiner@users.noreply.github.com>
2025-04-12 16:56:33 +0000

    delete previous files

--- example.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-"""Example Python file for demo."""
-
-def hello_world():
-    """Print hello world message."""
-    print("Hello, world!")
-
-if __name__ == "__main__":
-    hello_world()
