62035209f2d457a85bbc6ee9ec43c742f747cd6c
Josh Deiner <44216007+JoshDeiner@users.noreply.github.com>
2025-04-14 02:51:14 +0000

    remove util files

--- run-docker.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Run the Docker container with the current directory mounted
-docker run -it \
-  --name assistant-dev-container \
-  -v $(pwd):/app \
-  -v $(pwd)/data:/data \
-  assistant-dev "$@"
